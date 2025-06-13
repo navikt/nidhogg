998d5cdbb7a66fac3f39abf18431786b4a25984a
Petter Hannevold <petter.hannevold@nav.no>
2025-06-13 13:00:16 +0200

    Første controller

---
+++ src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggController.kt
@@ -0,0 +1,11 @@
+package no.nav.pensjon.nidhogg
+
+import org.springframework.stereotype.Controller
+
+@Controller
+class SporingsloggController {
+
+    fun hello(): String {
+        return "OK"
+    }
+}
