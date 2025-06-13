da575077587e62161ae8d928fac3325ad7689a79
Petter Hannevold <petter.hannevold@nav.no>
2025-06-13 14:02:59 +0200

    Første getter

--- src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggController.kt
+++ src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggController.kt
@@ -1,10 +1,12 @@
 package no.nav.pensjon.nidhogg
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SporingsloggController {
 
+    @GetMapping("/hello")
     fun hello(): String {
         return "OK"
     }
