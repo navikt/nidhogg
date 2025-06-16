a5c5b8c9c9388d18d3d4bdc3e9ef30550c8e3e14
Petter Hannevold <petter.hannevold@nav.no>
2025-06-16 13:21:38 +0200

    prøver å fikse at gh ikke finner kafka-q2.yaml, lagt til kafka-lytter

--- src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggAppender.kt
+++ src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggAppender.kt
@@ -1,6 +1,21 @@
 package no.nav.pensjon.nidhogg
 
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+import javax.sql.DataSource
 
-class SporingsloggAppender {
+@Component
+class SporingsloggAppender(
+    val dataSource: DataSource
+) {
 
+    @KafkaListener(topics = ["pensjon-q2.sporingslogg"])
+    fun listen(message: String) {
+        dataSource.connection.use { connection ->
+            connection.prepareStatement("INSERT INTO sporingslogg (melding) VALUES (?)").apply {
+                setString(1, message)
+                executeUpdate()
+            }
+        }
+    }
 }
