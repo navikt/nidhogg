38d2c96d618059fa27d8173e11d7775b110b20fd
Petter Hannevold <petter.hannevold@nav.no>
2025-06-16 09:42:41 +0200

    controller -> rest-controller

--- src/main/kotlin/no/nav/pensjon/nidhogg/NidhoggApplication.kt
+++ src/main/kotlin/no/nav/pensjon/nidhogg/NidhoggApplication.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.nidhogg
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class NidhoggApplication
--- src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggController.kt
+++ src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggController.kt
@@ -2,9 +2,10 @@ package no.nav.pensjon.nidhogg
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.sql.DataSource
 
-@Controller
+@RestController("/sporingslogg")
 class SporingsloggController(
     val dataSource: DataSource
 ) {
