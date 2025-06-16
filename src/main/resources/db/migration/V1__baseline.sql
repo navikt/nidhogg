12bfb6e9afb68076cd0dce7b4fb88564c7bcc744
Petter Hannevold <petter.hannevold@nav.no>
2025-06-16 13:35:02 +0200

    Overflødig komma i db-script

--- src/main/resources/db/migration/V1__baseline.sql
+++ src/main/resources/db/migration/V1__baseline.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS sporingslogg
 (
     id                       BIGSERIAL PRIMARY KEY,
     "tidspunkt"              TIMESTAMP   NOT NULL,
-    "innhold"                JSON        NOT NULL,
+    "innhold"                JSON        NOT NULL
     );
