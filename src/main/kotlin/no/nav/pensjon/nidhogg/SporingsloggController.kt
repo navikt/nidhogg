f2816b8e729a8ca7a90a038c9c329f780fb46bc7
Petter Hannevold <petter.hannevold@nav.no>
2025-06-16 10:05:45 +0200

    La til posgresql driver

--- src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggController.kt
+++ src/main/kotlin/no/nav/pensjon/nidhogg/SporingsloggController.kt
@@ -1,6 +1,5 @@
 package no.nav.pensjon.nidhogg
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.sql.DataSource
